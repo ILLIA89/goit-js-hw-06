0250ce912af699a35ba95b87abaa6bb0943830cf
ILLIA89 <anolij1@gmail.com>
2023-07-13 20:58:45 +0200

    task 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,24 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const ingredientsElUl = document.querySelector('ul#ingredients');
+// const listItems = ingredients.map((ingredient) => {
+
+//   const elementList = document.createElement('li');
+//   elementList.textContent = ingredient;
+//   elementList.classList.add('item');
+//   return elementList;
+  
+// });
+
+// ingredientsElUl.append(...listItems);
+
+
+
+// Вибираємо список ul за його id
+const ul = document.querySelector("#ingredients"); 
+// Створюємо розмітку зі списком інгредієнтів
+const markup = ingredients.map(ingredient => `<li class="item">${ingredient}</li>`).join("");
+// Вставляємо розмітку у список ul
+ul.insertAdjacentHTML("beforeend", markup); 

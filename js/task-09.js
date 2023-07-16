0ba9b1edce3a151764998c780cae7adfab8e8b41
ILLIA89 <anolij1@gmail.com>
2023-07-16 21:11:23 +0200

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,30 @@
+
+
+// 1. Спочатку за допомогою querySelector отримую посилання до елементів 
+// (".widget", ".color", ".change-color").
+// 2. Потім додаю слухача події 'click' на кнопку 'button.change-color'
+// 3. Далі в функції за допомогою const свторюю посилання на getRandomHexColor()
+// 4. Потім  style.backgroundColor присвоюю функцію getRandomHexColor
+// яка буде змінювати рандомно колір елемента body.
+// 5. І в spanColor за допомогою textContent
+//  виводжу назву кольору який генерується.
+
+const divWidget = document.querySelector('.widget');
+const spanColor = document.querySelector('.color');
+const buttonChangeColor = document.querySelector('.change-color');
+
+buttonChangeColor.addEventListener('click',() => {
+  const magicColor = getRandomHexColor();
+  document.body.style.backgroundColor = magicColor;
+  spanColor.textContent = magicColor;
+});
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+
+
